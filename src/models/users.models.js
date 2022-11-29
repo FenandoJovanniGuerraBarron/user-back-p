79581170931d5e7617926dd774dd2ac10f490f13
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2022-11-29 14:30:38 -0600

    validaciones

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -2,34 +2,34 @@ const { DataTypes } = require('sequelize')
 
 const db = require('../utils/database')
 
-const Users = db.define('users',{
-    id:{
-        type:DataTypes.INTEGER,
-        autoIncrement:true,
-        primaryKey:true
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
     },
-    first_name:{
-        type:DataTypes.STRING
+    first_name: {
+        type: DataTypes.STRING
     },
-    last_name:{
-        type:DataTypes.STRING
+    last_name: {
+        type: DataTypes.STRING
     },
-    email:{
-        type:DataTypes.STRING,
-        unique:true,
-        allowNull:true,
-        validate:{
-            isEmail:true
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true
         }
     },
-    password:{
-        type:DataTypes.STRING,
-        allowNull:true
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    birthday:{
-        type:DataTypes.DATE
+    birthday: {
+        type: DataTypes.DATE
     }
 })
 
 
-module.exports= Users
+module.exports = Users
